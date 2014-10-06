639841692839f2358e82042458ad4d764c4e6b08
tomduhourq <t.duhourq@gmail.com>
2014-10-06 09:24:26 -0300

    Added MissingElem from Codility lesson 01

--- src/main/scala/codility/lesson01/Frog.scala
+++ src/main/scala/codility/lesson01/Frog.scala
@@ -5,7 +5,7 @@ class Frog {
 	 * to the other side of the river by making jumps of size D. The frog is at position X and
 	 * has to travel to position Y. Count the minimal number of jumps that the frog has to make*/
   
-	def minimumJumps(x :Int,y :Int,d :Int) ={
-	  if(((y.toFloat-x.toFloat)/d.toFloat).toString.split('.')(1) != "0") (y-x)/d + 1 else  (y-x)/d
+	def minimumJumps(X :Int,Y :Int,D :Int) ={
+	  if(((Y-X)/D.toFloat).toString.split('.')(1) != "0") (Y-X)/D + 1 else (Y-X)/D
 	}
 }
