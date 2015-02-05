75662428959aa859b1e5bc2c140f7784afd8e37c
Tomás Duhourq <t.duhourq@gmail.com>
2015-02-05 16:27:51 -0300

    Added curry, uncurry and compose

--- src/main/scala/fp/Ch2.scala
+++ src/main/scala/fp/Ch2.scala
@@ -25,6 +25,15 @@ class Ch2 {
         gt(i, as(iPos + 1))
     }
 
-  // Exercise p.32 --> implement partial1
+  // Exercise p.29 --> implement partial1
   def partial1[A, B, C](a: A, f: (A, B) => C): B => C = b => f(a, b)
+  
+  // Exercise p.30 --> curry. Return a function that takes its arguments one by one
+  def curry[A,B,C](f: (A,B) => C): A =>(B => C) = a => b => f(a,b)
+  
+  // Exercise p.30 --> uncurry. Return a function that takes all of its arguments together.
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C = (a,b) => f(a)(b)
+  
+  // Exercise p.30 --> compose. Compose two functions to get only one by applying f after applying g
+  def compose[A,B,C](f: B => C, g:A => B): A => C = a => f(g(a))
 }
