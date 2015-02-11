cb7b715cc580cb58ddf3dbde61c72de7a47897cd
Tomás Duhourq <t.duhourq@gmail.com>
2015-02-11 10:51:49 -0300

    Add primes0 to Mathematics

--- src/main/scala/mathematics/Mathematics.scala
+++ src/main/scala/mathematics/Mathematics.scala
@@ -9,12 +9,14 @@ class Mathematics {
     n % i != 0
   }
 
-  lazy val primes: Stream[Int] = 2 #:: Stream.from(3, 2).filter(isPrime)
+  lazy val primes: Stream[Int] = 2 #:: 
+    Stream.from(3, 2).filter(isPrime)
   // This one determines 'in line' if the number to add is prime.
   lazy val primes2: Stream[Int] = 2 #:: primes2.map(i =>
-    Stream.from(i + 1).find(j =>
-      primes2.takeWhile(k => k * k <= j).forall(j % _ > 0)).get)
-      
-      
-  def firstNPrimes(n: Int) = primes.takeWhile(n>).toSet
+    Stream.from(i + 1).find(j => primes2.takeWhile(k => k * k <= j).forall(j % _ > 0)).get)
+  // Best implementation I could reach
+  lazy val primes0: Stream[Int] = 2 #:: 
+    Stream.from(3,2).filter{i => primes.takeWhile(j => j*j <= i).forall(i % _ != 0)}    
+    
+  def firstNPrimes(n: Int) = primes0.takeWhile(n>).toSet
 }
