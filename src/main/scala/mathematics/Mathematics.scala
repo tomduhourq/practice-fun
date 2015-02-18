0010cf779efafd7c40f5be6fdf72c5e612007186
Tomás Duhourq <t.duhourq@gmail.com>
2015-02-18 15:49:29 -0500

    Update Math on primes

--- src/main/scala/mathematics/Mathematics.scala
+++ src/main/scala/mathematics/Mathematics.scala
@@ -16,7 +16,11 @@ class Mathematics {
     Stream.from(i + 1).find(j => primes2.takeWhile(k => k * k <= j).forall(j % _ > 0)).get)
   // Best implementation I could reach
   lazy val primes0: Stream[Int] = 2 #:: 
-    Stream.from(3,2).filter{i => primes.takeWhile(j => j*j <= i).forall(i % _ != 0)}    
-    
+    Stream.from(3,2).filter{i => primes.takeWhile(j => j*j <= i).forall(i % _ != 0)} 
+  // Yet another tail Rec implementation
+  val primesTailRec: Stream[Int] = {
+    def generatePrimes (s: Stream[Int]): Stream[Int] = s.head #:: generatePrimes(s.tail filter (_ % s.head != 0))
+    generatePrimes(Stream.from(2))
+  }
   def firstNPrimes(n: Int) = primes0.takeWhile(n>).toSet
 }
