f842a16a6252c59b9e7e491fb076ea3d9d72c530
tomasduhourq <tomasduhourq@glb-d3206.globant.com>
2015-02-25 14:32:36 -0500

    Missing Integer from codility

---
+++ src/main/scala/codility/lesson02/MissingInteger.scala
@@ -0,0 +1,15 @@
+package codility.lesson02
+
+import scala.collection.immutable.SortedSet
+
+/**
+ * Correctness: 100%
+ * Performance: 100%
+ * Created by tomasduhourq on 2/25/15.
+ */
+object MissingInteger {
+  // Find the minimum missing number that doesn't occur in A
+  def solution(A: Array[Int]): Int =
+    // Consequent subtractions of the SortedSet of A (my seed would be the set)
+    A.foldLeft( SortedSet( (1 to (A.length + 1)).toSeq: _*) )(_ - _).headOption.getOrElse(1)
+}
