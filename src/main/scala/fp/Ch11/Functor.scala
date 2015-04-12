0840fcd8de6218c94104c7bdc698603c97bd8a98
Tomás <t.duhourq@gmail.com>
2015-04-11 22:31:12 -0300

    Introduce the Functor trait

---
+++ src/main/scala/fp/Ch11/Functor.scala
@@ -0,0 +1,14 @@
+package fp.Ch11
+
+/** Functor abstraction
+ *  representing a transformation
+ */
+trait Functor[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+  def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) =
+    (map (fab)(_._1), map (fab)(_._2))
+}
+
+object ListFunctor extends Functor[List]{
+  def map[A, B](as: List[A])(f: A => B) = as map f
+}
