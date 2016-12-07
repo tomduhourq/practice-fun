6d8a651262c39bcc998d7ab547f13661375ea72b
tomduhourq <t.duhourq@gmail.com>
2016-12-06 22:57:52 -0300

    Restart list

--- src/main/scala/fp/Ch2.scala
+++ src/main/scala/fp/Ch2.scala
@@ -2,7 +2,7 @@ package fp
 
 import scala.annotation.tailrec
 
-// This class represents Chapter 2 exercises of Functional Programming in Scala
+/** Chapter 2 exercises of Functional Programming in Scala */
 object Ch2 {
   val FIBS_INITIAL = Set(0, 1)
   // Exercise p.22 --> Provide the nth Fibonacci number with a tail recursive function
@@ -23,23 +23,22 @@ object Ch2 {
         gt(i, as(iPos + 1))
     }
 
+  def isSorted2[A](as: Array[A], ordered: (A, A) => Boolean): Boolean =
+    as zip as.tail forall { case (x, y) => ordered(x, y) }
+
   // NTH composeAll, apply a List of functions in a composite way
   def composeAll[A](fs: List[A => A]): A => A =
-    fs.foldLeft((a: A) => identity(a))(_ andThen _)
+    fs.foldLeft(identity[A](_))(_ andThen _)
 
   // Exercise p.29 --> implement partial1
-  def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
-    b => f(a, b)
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C = b => f(a, b)
   
   // Exercise p.30 --> curry. Return a function that takes its arguments one by one
-  def curry[A,B,C](f: (A,B) => C): A =>(B => C) =
-    a => b => f(a,b)
+  def curry[A,B,C](f: (A,B) => C): A =>(B => C) = a => b => f(a,b)
   
   // Exercise p.30 --> uncurry. Return a function that takes all of its arguments together.
-  def uncurry[A,B,C](f: A => B => C): (A,B) => C =
-    (a,b) => f(a)(b)
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C = (a,b) => f(a)(b)
   
   // Exercise p.30 --> compose. Compose two functions to get only one by applying f after applying g
-  def compose[A,B,C](f: B => C, g:A => B): A => C =
-    a => f(g(a))
+  def compose[A,B,C](f: B => C, g:A => B): A => C = a => f(g(a))
 }
--- src/main/scala/fp/Ch3/List.scala
+++ src/main/scala/fp/Ch3/List.scala
@@ -1,155 +1,68 @@
 package fp.Ch3
 
-/**
- * Created by tomas on 22/02/15.
- */
-class Lists {
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
-  // Exercise 2 p.40 --> tail. Retrieve the tail from a List.
-  def tail[A](list: List[A]): List[Any] = list match {
-    case Nil => Nil
-    case x :: xs => xs
-  }
+/** Chapter 3 */
+object List {
 
-  // Exercise 3 p.40 --> drop. Remove the first n elements of a list.
-  def drop[A](list: List[A], n: Int): List[A] = list match {
-    case Nil => Nil
-    case x :: xs if n > 0 => drop(xs, n - 1)
-    case l@x :: xs if n == 0 => l
+  /**
+   * Make possible for our lists to retrieve the product of its members
+   *
+   * @param list the list of elements to take the product
+   * @tparam A the type parameter bounded to Numeric
+   * @return An A representing the product
+   */
+  def product[A : Numeric](list: List[A]): A = {
+    val numeric = implicitly[Numeric[A]]
+    @annotation.tailrec
+    def productRec(acum: A, left: List[A]): A = {
+      left match {
+        case Nil => acum
+        case Cons(head, tail) => productRec(numeric.times(head, acum), tail)
+      }
+    }
+    productRec(numeric.one, list)
   }
 
   /**
-   * Exercise 4 p.40 --> dropWhile. Removes the elements from the List prefix as long as they match a predicate.
-   * By passing the arguments one by one, we are returning a function that takes f separately in order to help the
-   * compiler infer the types of f and not having to do something like (x: Int) => x > 45
+   * Make possible for our lists to retrieve the sum of its members
+   *
+   * @param list the list of elements to take the sum
+   * @tparam A the type parameter bounded to Numeric
+   * @return An A representing the sum
    */
-  def dropWhile[A](list: List[A])(f: A => Boolean): List[A] = list match {
-    case Nil => Nil
-    case x :: xs if f(x) => dropWhile(xs)(f)
-    case l @ x :: xs if !f(x) => l
-  }
-
-  // Exercise 5 p.41 --> setHead. Replace the first element of a List with a different value.
-  def setHead[A](list: List[A], elem: A): List[A] = list match {
-    case Nil => List(elem)
-    case x :: xs => elem :: xs
-  }
-
-  // NTH --> append. Append listA to ListB. Its runtime is determined only by the length of a1.
-  def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
-    case Nil => a2
-    case x :: xs => x :: append(xs, a2)
-  }
-
-  // Exercise 6 p.42 --> init. Retrieve all the elements except the last one from a list.
-  def init[A](list: List[A]): List[A] = list match {
-    case Nil => Nil
-    case x :: Nil => Nil
-    case x :: xs => x :: init(xs)
-  }
-
-  // NTH --> foldRight. Perform a series of operations recursively on a List to get a final result.
-  // Note that z is the seed of the operation represented by f, and is the last operand to be evaluated.
-  def foldRight[A, B](l: List[A], z: B)(f: (A, B) => B): B = l match {
-    case Nil => z
-    case x :: xs => f(x, foldRight(xs, z)(f))
-  }
-
-  // Exercise 9 p.44 --> length. Count the elements of a List using foldRight.
-  def length[A](l: List[A]): Int = foldRight(l, 0)((a, acum) => 1 + acum)
-
-  // Exercise 10 p.44 --> foldLeft. Note that in foldLeft the seed is the acummulator
-  // and it's also tail-recursive.
-  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
-    case Nil => z
-    case x :: xs => foldLeft(xs, f(z, x))(f)
-  }
-
-  // Exercise 11 p.44 --> sum,product and length using foldLeft.
-  def sum(l: List[Int]): Int = foldLeft(l, 0)(_ + _)
-
-  def product(l: List[Int]): Int = foldLeft(l, 1)(_ * _)
-
-  def length2[A](l: List[A]): Int = foldLeft(l, 0)((acum, a) => acum + 1)
-
-  // Exercise 12 p.45 --> reverse.
-  def reverse[A](l: List[A]): List[A] = foldLeft(l, Nil: List[A])((xs, ys) => ys :: xs)
-
-  def reverse2[A](l: List[A]): List[A] = foldRight(l, Nil: List[A])((xs: A, ys: List[A]) => ys ::: List(xs))
-
-  // NTH --> reverse: compute a List in a reversed way
-  def reverse3[A](l: List[A]): List[A] = l match {
-    case Nil => Nil
-    case x :: xs => reverse(xs) :+ x
-  }
-
-  // Exercise 13 p.45 --> implement foldRight in terms of foldLeft and viceversa.
-  def foldLeft2[A, B](l: List[A], z: B)(f: (B, A) => B): B = foldRight(l, (b: B) => b)((a, g) => b => g(f(b, a)))(z)
-
-  def foldRight2[A, B](l: List[A], z: B)(f: (A, B) => B): B = foldLeft(l, (b: B) => b)((g, a) => b => g(f(a, b)))(z)
-
-  // Exercise 14 p.45 --> implement append in terms of foldLeft or foldRight
-  def appendLeft[A, B](l1: List[A], l2: List[A]): List[A] = foldLeft(l2, l1)((b, a) => b ::: List(a))
-
-  def appendRight[A, B](l1: List[A], l2: List[A]): List[A] = foldRight(l2, l1)((a, b) => b ::: List(a))
-
-  // Exercise 15 p.45 --> flatten. Concatenate a list of lists into a single list (trying to use things we have done).
-  def flatten[A](l: List[List[A]]): List[A] = foldLeft(l, Nil: List[A])(appendLeft)
-
-  def flattenInverse[A](l: List[List[A]]): List[A] = foldRight(l, Nil: List[A])(appendRight)
-  
-  def flattenEasy[A](l: List[List[A]]): List[A] = for(xs <- l; x <- xs) yield x
-
-  // Exercise 16 p.45 --> addOne. Adds 1 to each element of a List.
-  def addOne[A](l: List[Int]): List[Int] = l.map(_ + 1)
-
-  // Exercise 17 p.46 --> convToString: Convert a List[Double] into List[String]
-  def convToString(l: List[Double]): List[String] = l map (_.toString)
-
-  // Exercise 18 p.46 --> map: apply a function to each element of a List and return a new one.
-  def map[A, B](l: List[A])(f: A => B): List[B] = foldLeft(l, Nil: List[B])((b, a) => b ::: List(f(a)))
-
-  // Exercise 19. p.46 --> filter: removes the elements from a List unless they satisfy the given function.
-  def filter[A](l: List[A])(f: A => Boolean): List[A] = l match {
-    case Nil => Nil
-    case x :: xs if f(x) => x :: filter(xs)(f)
-    case x :: xs if !f(x) => filter(xs)(f)
-  }
-
-  // Exercise 20 p.46 --> flatMap : map all elements of a List to Lists and flatten it
-  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = flatten(map(l)(f))
-
-  // Exercise 21 p.46 --> filter2: implement filter in terms of flatMap
-  def filter2[A](l: List[A])(f: A => Boolean): List[A] = flatMap(l)(a => if (f(a)) List(a) else Nil)
-
-  // Exercise 22 p.46 --> addLists: add the elements of both lists.
-  def addLists(l1: List[Int], l2: List[Int]): List[Int] = {
-    if (l1.length == l2.length) {
-      l1 match {
-        case Nil => Nil
-        case x :: xs => {
-          val pos = l1.indexOf(x)
-          x + l2(pos) :: addLists(xs, l2.drop(1))
-        }
+  def sum[A : Numeric](list: List[A]): A = {
+    val numeric = implicitly[Numeric[A]]
+    @annotation.tailrec
+    def sumRec(acum: A, left: List[A]): A = {
+      left match {
+        case Nil => acum
+        case Cons(head, tail) => sumRec(numeric.plus(head, acum), tail)
       }
     }
-    else Nil
+    sumRec(numeric.zero, list)
   }
 
-  // Exercise 23 p.46 --> generalizeAction: the last function is called zipWith in Scala's List API.
-  def zipWith[A](l1: List[A], l2: List[A])(f: (A, A) => A): List[A] = (l1, l2) match {
-    case (Nil, _) => Nil
-    case (_, Nil) => Nil
-    case (x :: xs, y :: ys) => f(x, y) :: zipWith(xs, ys)(f)
+  /**
+   * This would be the equivalent of (Num a) => [a] -> (a -> a -> a) -> a -> a
+   *
+   * @param list the list of As
+   * @param f the function to apply to acum and head (BE AWARE of the order)
+   * @param zero the zero element of the function
+   * @tparam A the type of the list
+   * @return an A produced by subsequently applying the function
+   */
+  def op[A : Numeric](list: List[A], f: (A, A) => A, zero: A): A = {
+    @annotation.tailrec
+    def opRec(acum: A, left: List[A]): A = {
+      left match {
+        case Nil => acum
+        case Cons(head, tail) => opRec(f(acum, head), tail)
+      }
+    }
+    opRec(zero, list)
   }
 
-  // Exercise 24 p.47 --> hasSubsequence: determine if a List contains a given subsequence.
-  def hasSubsequence[A](l: List[A], sub: List[A]): Boolean = (l, sub) match {
-    case (Nil, Nil) => true
-    case (Nil, _) => false
-    // This is the cut condition
-    case (x :: xs, Nil) => true
-    case (x :: xs, y :: ys) if x == y => hasSubsequence(xs, ys)
-    case (x :: xs, y :: ys) if x != y => hasSubsequence(xs, sub)
-  }
 }
